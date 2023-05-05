35ba1d3354cab48d75f926f5d829276ca78c38e7
Quartet <loboda3002@gmial.com>
2023-05-05 11:21:15 +0300

    Lab 4. Redid through the list and method

--- 4/Program.cs
+++ 4/Program.cs
@@ -2,35 +2,40 @@
 {
     internal class Program
     {
+        static int vowels, consonant;
+        static List<char> vowelsList     = new List<char>() { 'A', 'E', 'I', 'O', 'U' };
+        static List<char> consonantsList = new List<char>() { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z' };
+
+        static void Count(string text) {
+            foreach (char symbol in text) {
+                if (vowelsList.Contains(symbol)) {
+                    vowels++;
+                } else if (consonantsList.Contains(symbol)) {
+                    consonant++;
+                }
+            }
+        }
         static void Main(string[] args) {
             string text;
             string filename;
             if (args.Length == 0) {
                 filename = "input.txt";
-            } else
-            {
-                filename = args[0];  
+            } else {
+                filename = args[0];
             }
-            
+
             try {
                 text = File.ReadAllText($@"{Environment.CurrentDirectory}\{filename}");
             } catch {
                 Console.WriteLine("File does not exist!");
                 return;
             }
-            char[] symbols = text.ToLower().ToCharArray(); // convert to char array
+            text = text.ToUpper();
 
-            int vowels = 0, consonant = 0;
-            foreach (char symbol in symbols) {
-                if (Char.IsLetter(symbol)) {
-                    if ("aeiou".IndexOf(symbol) != -1) {
-                        vowels++;
-                    }
-                    else {
-                        consonant++;
-                    }
-                }
-            }
+            vowels = 0;
+            consonant = 0;
+
+            Count(text);
 
             Console.WriteLine($"Vowels: {vowels}\nConsonants: {consonant}");
         }
